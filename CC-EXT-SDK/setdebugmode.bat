1394d9e2c60bc27d33f032afabde245e997db10a
Taracque <taracque@imac.hu>
2016-01-08 19:35:50 +0100

    Added CC 2015 debug mode…

--- CC-EXT-SDK/setdebugmode.bat
+++ CC-EXT-SDK/setdebugmode.bat
@@ -1,5 +1,13 @@
 @echo off
 reg query HKEY_CURRENT_USER\Software\Adobe\CSXS.5 /v PlayerDebugMode
-if %errorlevel%==1 GOTO INSTALL
-:INSTALL
+if %errorlevel%==1 GOTO INSTALL5
+GOTO NEXT5
+:INSTALL5
 reg add HKEY_CURRENT_USER\Software\Adobe\CSXS.5 /v PlayerDebugMode /t REG_SZ /d 1
+:NEXT5
+reg query HKEY_CURRENT_USER\Software\Adobe\CSXS.6 /v PlayerDebugMode
+if %errorlevel%==1 GOTO INSTALL6
+GOTO NEXT6
+:INSTALL6
+reg add HKEY_CURRENT_USER\Software\Adobe\CSXS.6 /v PlayerDebugMode /t REG_SZ /d 1
+:NEXT6
--- CC-EXT-SDK/setdebugmode.sh
+++ CC-EXT-SDK/setdebugmode.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
-/usr/libexec/PlistBuddy -c "Add:PlayerDebugMode string 1" ~/Library/Preferences/com.adobe.CSXS.5.plist
+if [ -f ~/Library/Preferences/com.adobe.CSXS.5.plist ]; then
+	/usr/libexec/PlistBuddy -c "Add:PlayerDebugMode string 1" ~/Library/Preferences/com.adobe.CSXS.5.plist
+fi
+if [ -f ~/Library/Preferences/com.adobe.CSXS.6.plist ]; then
+	/usr/libexec/PlistBuddy -c "Add:PlayerDebugMode string 1" ~/Library/Preferences/com.adobe.CSXS.6.plist
+fi
 #must kill cfprefsd to take effect
 ps -ef | grep "cfprefsd" | awk '{print $2}' | xargs kill
